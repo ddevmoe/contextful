1864f15d6f4a293776128d4d92dc2369cea37a45
Moe Dev <ddevmoe@gmail.com>
2023-12-25 02:24:01 +0200

    Added an option to set context to an empty dict when one is not provided.

--- contextful/context_logger.py
+++ contextful/context_logger.py
@@ -31,21 +31,49 @@ _global_context = ContextVar('context_logger_asyncio_context_store', default={})
 
 
 class ContextLogger(logging.Logger):
-    def __init__(self, name: str = 'main', level: int = logging.NOTSET):
+    def __init__(
+        self,
+        name: str = 'main',
+        level: int = logging.NOTSET,
+        always_set_context: bool = False,
+    ):
+        """
+        always_set_context - Ensure `LogRecord` instances always have a `context` attribute set to an empty `dict`, even when no context is provided.
+        """
+
+        self._always_set_context = always_set_context
         super().__init__(name, level)
 
-    def _log(self, level: int, msg: object, *args, extra: dict | None = None, **kwargs) -> None:
-        current_context = _global_context.get()
-        context_wrapper = {'context': current_context}
-        return super()._log(level, msg, *args, extra=context_wrapper, **kwargs)
+    def _log(self, level: int, msg: object, context: dict | None = None, *args, **kwargs):
+        if not self.isEnabledFor(level):
+            return
+
+        finalized_context = {**_global_context.get(), **(context or {})}
+
+        # If there's both the global context and the current log's context are empty - log without context
+        if not finalized_context and not self._always_set_context:
+            super()._log(level, msg, args, **kwargs)
+            return
+
+        # If extra isn't utilized by the current log, add a new one with our context mounted on top
+        if 'extra' not in kwargs:
+            super()._log(level, msg, args, **kwargs, extra={'context': finalized_context})
+            return
+
+        extra = kwargs.get('extra', {})
+        if 'context' in extra:
+            raise ValueError('Logging "extra" dict must not contain a key named "context" as it is reserved for ContexLogger\'s usage.')
+        extra['context'] = finalized_context
+
+        super()._log(level, msg, args, **kwargs)
 
     def _remove_context(self, log_context: LogContext):
         _global_context.reset(log_context.context_token)
 
-    def context(self, extra: dict) -> LogContext:
+    def context(self, context: dict) -> LogContext:
         current_context = _global_context.get()
-        new_context = {**current_context, **extra}
+        new_context = {**current_context, **context}
         context_token = _global_context.set(new_context)
 
-        log_context = LogContext(extra, context_token, self._remove_context)
+        log_context = LogContext(context, context_token, self._remove_context)
         return log_context
